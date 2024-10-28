83d289db57e5f5bff120223f094178fd3a447532
Nikolai <energy1224@gmail.com>
2024-10-28 08:56:27 +0300

    Созданы две функции

--- main.go
+++ main.go
@@ -1,8 +1,27 @@
 package main
 
+import "fmt"
+
 func main() {
 	const USDeur = 0.92
 	const USDrub = 96
 	const EURrub = USDrub / USDeur
 
+	inputSumm := getUserInput()
+
+	fmt.Print(inputSumm)
+
+}
+func getUserInput() float64 {
+	var inputSumm float64
+	fmt.Print("_Введите сумму_")
+	fmt.Scan(&inputSumm)
+	return inputSumm
+	
+
+}
+
+func calculate(baseCurrency float64, targetCurrency float64, requiredSumm float64 )float64 {
+        var result float64
+	return result
 }
